c9e05758bc977a4f7cfd263d094299c0a9e8653d
Toshiaki Seino <st12318@gmail.com>
2018-07-16 02:37:02 +0900

    Add Source class

---
+++ rplugin/python3/deoplete/source/ruby-langserv.py
@@ -0,0 +1,28 @@
+import pathlib
+import json
+import re
+import subprocess
+
+from deoplete.util import getlines, expand
+from .base import Base
+from solargraph.client import Client
+
+
+class Source(Base):
+    def __init__(self, vim):
+        self.name = "ruby-langserv"
+        self.filetypes = ['ruby']
+        self.mark = '[rb-langserv]'
+        self.rank = 900
+        self.input_pattern = r'\.[a-zA-Z0-9_?!]+|[a-zA-Z]\w*::\w*'
+        self.langserv_status = False
+        super(Source, self).__init__(vim)
+
+    def on_init(self, context):
+        pass
+
+    def get_complete_position(self, context):
+        pass
+
+    def gather_candidates(self, context):
+        pass
